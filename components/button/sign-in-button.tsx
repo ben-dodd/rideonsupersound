154c511795d14b416dcf1f536b014f465ae84564
Ben D <thebillham@gmail.com>
2021-08-19 21:28:41 +1200

    google

--- components/button/sign-in-button.tsx
+++ components/button/sign-in-button.tsx
@@ -4,7 +4,7 @@ export default function SignInButton() {
   return (
     <button
       className="bg-black hover:bg-gray-900 text-white mx-auto rounded px-8 py-2 mt-4 mb-8 transform hover:shadow-2xl"
-      onClick={() => signIn()}
+      onClick={() => signIn("google")}
     >
       Sign In
     </button>
