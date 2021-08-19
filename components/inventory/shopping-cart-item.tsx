2f858a93280e61513f4ee052e0c405fa52a26895
Ben D <thebillham@gmail.com>
2021-08-20 00:24:20 +1200

    sc items

--- components/inventory/shopping-cart-item.tsx
+++ components/inventory/shopping-cart-item.tsx
@@ -24,16 +24,17 @@ export default function SellItem({ id, cartItem }: ShoppingCartItemProps) {
   }, [inventory]);
 
   return (
-    <div className="flex w-full bg-black border-b border-white text-white">
+    <div className="flex w-full bg-black border-b border-white text-white relative">
       <img
-        className="w-16 h-16"
+        className="w-20 h-20"
         src={item?.image_url || "/img/default.png"}
         alt={item?.title || "Inventory image"}
       />
+      <div className="absolute w-20 h-8 bg-opacity-50 bg-black text-white flex justify-center items-center text-sm">
+        {getItemSku(item)}
+      </div>
       <div className="flex flex-col w-full p-2 justify-between">
-        <div className="text-xs pl-2">{`[${getItemSku(item)}] ${getItemTitle(
-          item
-        )}`}</div>
+        <div className="text-xs pl-1">{getItemTitle(item)}</div>
         <div className="text-red-500 self-end">
           {getCartItemSummary(item, cartItem)}
         </div>
