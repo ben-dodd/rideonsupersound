6d9249af54436671b94d1b4f2b3270d510fb90fa
Ben D <thebillham@gmail.com>
2021-09-13 22:44:42 +1200

    lay

--- components/sell/index.tsx
+++ components/sell/index.tsx
@@ -38,35 +38,35 @@ export default function SellScreen() {
 
   return (
     <div className="flex relative overflow-x-hidden" {...handlers}>
-      <div className={`bg-blue-200 w-2/3`}>
+      <div className={`bg-blue-200 w-full sm:w-2/3`}>
         <SearchBar />
         <InventoryScroll />
       </div>
       <div
         className={`absolute top-0 transition-offset duration-300 ${
           showCart ? "left-0" : "left-full"
-        } bg-yellow-200 w-1/3 transition-none static h-menu`}
+        } h-full w-full bg-yellow-200 sm:w-1/3 sm:transition-none sm:static sm:h-menu`}
       >
         <ShoppingCart />
       </div>
       <div
         className={`absolute top-0 transition-offset duration-300 ${
           showHold ? "left-0" : "left-full"
-        } bg-yellow-200 w-1/3 transition-none static h-menu`}
+        } h-full w-full bg-yellow-200 sm:w-1/3 sm:transition-none sm:static sm:h-menu`}
       >
         <HoldScreen />
       </div>
       <div
         className={`absolute top-0 transition-offset duration-300 ${
           showCreateContact?.id ? "left-0" : "left-full"
-        } bg-yellow-200 w-1/3 transition-none static h-menu`}
+        } h-full w-full bg-yellow-200 sm:w-1/3 sm:transition-none sm:static sm:h-menu`}
       >
         <CreateContactScreen />
       </div>
       <div
         className={`absolute top-0 transition-offset duration-300 ${
-          showSaleScreen ? "left-0" : "left-1/2"
-        } bg-yellow-200 w-1/3 transition-none static h-menu`}
+          showSaleScreen ? "left-0" : "left-full"
+        } h-full w-full bg-yellow-200 sm:w-1/3 sm:transition-none sm:static sm:h-menu`}
       >
         <SaleScreen />
       </div>
