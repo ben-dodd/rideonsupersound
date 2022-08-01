39f2fe21e892615336e37eaa3ca585f13658ccd7
Ben <8870073+ben-dodd@users.noreply.github.com>
2022-08-01 23:01:02 +1200

    Fix display min

--- components/nav/actions/inventory.tsx
+++ components/nav/actions/inventory.tsx
@@ -1,20 +1,63 @@
-import { useAtom } from 'jotai'
 import {
   clerkAtom,
   compactModeAtom,
   tableModeAtom,
   viewAtom,
 } from '@/lib/atoms'
+import { useAtom } from 'jotai'
 
-import TableIcon from '@mui/icons-material/TableView'
-import CompactIcon from '@mui/icons-material/ViewCompact'
+import { saveSystemLog } from '@/lib/db-functions'
+
+import ReturnIcon from '@mui/icons-material/AssignmentReturn'
+import ReceiveIcon from '@mui/icons-material/AssignmentReturned'
+import PrintIcon from '@mui/icons-material/Print'
 
 export default function InventoryNavActions() {
   const [tableMode, setTableMode] = useAtom(tableModeAtom)
   const [compactMode, setCompactMode] = useAtom(compactModeAtom)
+  const [view, setView] = useAtom(viewAtom)
+  const [clerk] = useAtom(clerkAtom)
   return (
     <div className="flex">
       <button
+        className="icon-text-button"
+        onClick={() => {
+          saveSystemLog('Inventory Nav - Receive stock clicked.', clerk?.id)
+          setView({
+            ...view,
+            receiveStockScreen: true,
+            returnStockScreen: false,
+          })
+        }}
+      >
+        <ReceiveIcon className="mr-1" />
+        Receive Items
+      </button>
+      <button
+        className="icon-text-button"
+        onClick={() => {
+          saveSystemLog('Inventory Nav - Return stock clicked.', clerk?.id)
+          setView({
+            ...view,
+            returnStockScreen: true,
+            receiveStockScreen: false,
+          })
+        }}
+      >
+        <ReturnIcon className="mr-1" />
+        Return Items
+      </button>
+      <button
+        className="icon-text-button"
+        onClick={() => {
+          saveSystemLog('Inventory Nav - Print labels clicked.', clerk?.id)
+          setView({ ...view, labelPrintDialog: true })
+        }}
+      >
+        <PrintIcon className="mr-1" />
+        Print Labels
+      </button>
+      {/* <button
         onClick={() => setCompactMode(!compactMode)}
         className={`text-brown-dark hover:text-brown mr-2 ${
           !compactMode && 'opacity-50'
@@ -29,7 +72,7 @@ export default function InventoryNavActions() {
         }`}
       >
         <TableIcon />
-      </button>
+      </button> */}
     </div>
   )
 }
--- pages/api/get-stock-display-min.ts
+++ pages/api/get-stock-display-min.ts
@@ -1,11 +1,11 @@
-import { NextApiHandler } from "next";
-import { query } from "../../lib/db";
+import { NextApiHandler } from 'next'
+import { query } from '../../lib/db'
 
 const handler: NextApiHandler = async (req, res) => {
-  const { k } = req.query;
+  const { k } = req.query
   try {
     if (!k || k !== process.env.NEXT_PUBLIC_SWR_API_KEY)
-      return res.status(401).json({ message: "Resource Denied." });
+      return res.status(401).json({ message: 'Resource Denied.' })
     const results = await query(
       `
       SELECT
@@ -20,14 +20,16 @@ const handler: NextApiHandler = async (req, res) => {
       FROM stock
       WHERE NOT is_deleted
       AND NOT is_misc_item
+      AND NOT is_misc_item IS NULL
       AND NOT is_gift_card
+      AND NOT is_gift_card IS NULL
       `
-    );
+    )
 
-    return res.json(results);
+    return res.json(results)
   } catch (e) {
-    res.status(500).json({ message: e.message });
+    res.status(500).json({ message: e.message })
   }
-};
+}
 
-export default handler;
+export default handler
