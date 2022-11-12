b6dbccab78e8bbdc4181213fa4de712af52c13bf
Ben <8870073+ben-dodd@users.noreply.github.com>
2022-11-12 15:35:35 +1300

    Redirect

--- app/AuthContext.tsx
+++ app/AuthContext.tsx
@@ -1,16 +1,21 @@
 'use client'
 
 import { Auth0Provider } from '@auth0/auth0-react'
+import { useEffect } from 'react'
 export interface AuthContextProps {
   children: React.ReactNode
 }
 
 export default function AuthContext({ children }: AuthContextProps) {
+  let redirectUri = 'http://localhost:3000'
+  useEffect(() => {
+    redirectUri = window.location.origin
+  }, [])
   return (
     <Auth0Provider
       domain={process.env.NEXT_PUBLIC_AUTH0_ISSUER}
       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
-      redirectUri={window.location.origin}
+      redirectUri={redirectUri}
     >
       {children}
     </Auth0Provider>
--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,9 @@ import { useAuth0 } from '@auth0/auth0-react'
 export default function IndexPage() {
   const { user, isAuthenticated, isLoading, loginWithRedirect } = useAuth0()
   // const { account } = useAccount(user?.email)
-  useRegisterID()
-  loginWithRedirect()
+  console.log(user)
+  // useRegisterID()
+  !isAuthenticated && !user && !isLoading && loginWithRedirect()
 
   return (
     <>
