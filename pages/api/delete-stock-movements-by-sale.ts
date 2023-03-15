fba33c2f8a0bdd11b2dd52cb961622db9ab6db36
Ben <8870073+ben-dodd@users.noreply.github.com>
2023-03-15 18:28:33 +1300

    Stock movements deleted completely

--- pages/api/delete-stock-movements-by-sale.ts
+++ pages/api/delete-stock-movements-by-sale.ts
@@ -8,12 +8,12 @@ const handler: NextApiHandler = async (req, res) => {
     if (!k || k !== process.env.NEXT_PUBLIC_SWR_API_KEY)
       return res.status(401).json({ message: 'Resource Denied.' })
     const results = await query(
-      // `
-      // DELETE FROM stock_movement WHERE sale_id = ?
-      // `,
       `
-      UPDATE stock_movement SET is_deleted = 1 WHERE sale_id = ?
+      DELETE FROM stock_movement WHERE sale_id = ?
       `,
+      // `
+      // UPDATE stock_movement SET is_deleted = 1 WHERE sale_id = ?
+      // `,
       [sale_id]
     )
     return res.json(results)
