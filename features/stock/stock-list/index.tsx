724f405a17cf88fead35c26b64750bf17d22da49
Ben <8870073+ben-dodd@users.noreply.github.com>
2023-03-28 00:08:48 +1300

    Add limit to stock list

--- features/stock/stock-list/index.tsx
+++ features/stock/stock-list/index.tsx
@@ -2,6 +2,7 @@ import SearchInput from 'components/inputs/search-input'
 import { useStockList } from 'lib/api/stock'
 import { useAppStore } from 'lib/store'
 import { Pages } from 'lib/store/types'
+import { useState } from 'react'
 import StockListItem from './stock-list-item'
 
 const StockList = () => {
@@ -10,8 +11,12 @@ const StockList = () => {
     stockPage: { searchBar },
     setSearchBar,
   } = useAppStore()
+  const [limit, setLimit] = useState(50)
 
   const handleSearch = (e) => setSearchBar(Pages.stockPage, e.target.value)
+  const filteredList = stockList?.filter?.((stockItem) =>
+    `${stockItem?.artist} ${stockItem?.title}`?.toUpperCase?.()?.includes(searchBar?.toUpperCase()),
+  )
 
   return (
     <div className="h-content overflow-y-scroll">
@@ -19,13 +24,17 @@ const StockList = () => {
         <SearchInput searchValue={searchBar} handleSearch={handleSearch} />
       </div>
       <div className="px-2">
-        {stockList
-          ?.filter?.((stockItem) =>
-            `${stockItem?.artist} ${stockItem?.title}`?.toUpperCase?.()?.includes(searchBar?.toUpperCase()),
-          )
-          ?.map((stockItem) => (
-            <StockListItem key={stockItem?.id} item={stockItem} />
-          ))}
+        {filteredList?.slice(0, limit)?.map((stockItem) => (
+          <StockListItem key={stockItem?.id} item={stockItem} />
+        ))}
+        {limit < filteredList?.length && (
+          <button
+            className="my-2 p-2 mx-auto w-full border hover:bg-gray-100"
+            onClick={() => setLimit((limit) => limit + 50)}
+          >
+            LOAD MORE...
+          </button>
+        )}
       </div>
     </div>
   )
