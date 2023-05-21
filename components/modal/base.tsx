cebb3a19f4553a46dba3c15ee0671184c58d5540
Ben <8870073+ben-dodd@users.noreply.github.com>
2023-05-21 20:43:20 +1200

    Animated modals

--- components/modal/base.tsx
+++ components/modal/base.tsx
@@ -1,3 +1,15 @@
+import { motion, Variants } from 'framer-motion'
+
+const backdropVariants: Variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+}
+
+const modalVariants: Variants = {
+  hidden: { scale: 0 },
+  visible: { scale: 1 },
+}
+
 export default function ModalBase({
   open = false,
   onClose = null,
@@ -6,13 +18,28 @@ export default function ModalBase({
   children,
 }) {
   return (
-    <div
+    <motion.div
       className={`${
         !open ? 'opacity-0 pointer-events-none ' : ''
-      }absolute transition-opacity duration-200 ease-in-out w-screen h-main top-0 left-0 flex items-center justify-center`}
+      }absolute w-screen h-main top-0 left-0 flex items-center justify-center`}
+      initial="hidden"
+      animate={open ? 'visible' : 'hidden'}
+      variants={backdropVariants}
+      transition={{ duration: 0.3, ease: 'easeInOut' }}
     >
-      <div onClick={disableBackdropClick ? null : onClose} className="absolute w-full h-full bg-black opacity-50" />
-      <div className={`bg-white w-11/12 ${width} mx-auto z-50 rounded-md shadow-2xl`}>{children}</div>
-    </div>
+      <motion.div
+        onClick={disableBackdropClick ? null : onClose}
+        className="absolute w-full h-full bg-black opacity-50"
+      />
+      <motion.div
+        className={`bg-white w-11/12 ${width} mx-auto z-50 rounded-md shadow-2xl`}
+        initial="hidden"
+        animate={open ? 'visible' : 'hidden'}
+        variants={modalVariants}
+        transition={{ duration: 0.1, ease: 'easeInOut' }}
+      >
+        {children}
+      </motion.div>
+    </motion.div>
   )
 }
