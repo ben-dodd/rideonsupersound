08f56d53729d2bd6a08e9751acea96a2dcfd8498
Ben <8870073+ben-dodd@users.noreply.github.com>
2023-06-11 17:49:06 +1200

    Remove date sale opened from getSalesJoin

--- pages/api/get-sales-join.ts
+++ pages/api/get-sales-join.ts
@@ -1,11 +1,11 @@
-import { NextApiHandler } from "next";
-import { query } from "../../lib/db";
+import { NextApiHandler } from 'next'
+import { query } from '../../lib/db'
 
 const handler: NextApiHandler = async (req, res) => {
-  const { customer_id, k } = req.query;
+  const { customer_id, k } = req.query
   try {
     if (!k || k !== process.env.NEXT_PUBLIC_SWR_API_KEY)
-      return res.status(401).json({ message: "Resource Denied." });
+      return res.status(401).json({ message: 'Resource Denied.' })
     const results = await query(
       `
       SELECT
@@ -19,7 +19,6 @@ const handler: NextApiHandler = async (req, res) => {
         stock_price.vendor_cut,
         stock_price.total_sell,
         stock_price.date_valid_from AS date_price_valid_from,
-        sale.date_sale_opened,
         sale.date_sale_closed,
         stock.vendor_id,
         sale.store_cut,
@@ -39,14 +38,14 @@ const handler: NextApiHandler = async (req, res) => {
       AND sale_item.is_deleted = 0
       `,
       customer_id
-    );
+    )
 
-    return res.json(results);
+    return res.json(results)
   } catch (e) {
-    res.status(500).json({ message: e.message });
+    res.status(500).json({ message: e.message })
   }
-};
+}
 
-export default handler;
+export default handler
 
 // -- LEFT JOIN (SELECT MAX(date_valid_from) AS date_valid_from, stock_id, vendor_cut, total_sell FROM stock_price GROUP BY stock_id) stock_price
