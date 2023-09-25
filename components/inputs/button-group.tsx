cdcb85f4ca8bceffd1db8ff7a2568c8f78ab7448
Ben <8870073+ben-dodd@users.noreply.github.com>
2023-09-25 20:14:16 +1300

    Button group fix

--- components/inputs/button-group.tsx
+++ components/inputs/button-group.tsx
@@ -8,12 +8,12 @@ const ButtonGroup = ({ items, value, onChange, className = '' }) => {
   }
   useEffect(() => setSelected(value), [value])
   return (
-    <div className={`rounded-lg border ${className}`}>
-      <div className="flex grow w-full h-full">
+    <div className={`rounded-lg border ${className} shadow-md`}>
+      <div className="flex w-full h-full">
         {items?.map((item) => (
           <div
             key={item?.key}
-            className={`p-2 cursor-pointer ${
+            className={`p-2 flex-grow text-center cursor-pointer ${
               selected === item?.key ? 'bg-green-200 hover:bg-green-300' : 'hover:bg-green-100'
             }`}
             onClick={() => handleClickSelect(item)}
