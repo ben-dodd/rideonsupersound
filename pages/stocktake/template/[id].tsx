d346517dc1bd3938ea307c4b8f7753dde5ac6707
Ben <8870073+ben-dodd@users.noreply.github.com>
2023-10-22 12:55:33 +1300

    Stocktake pages

--- pages/stocktake/template/[id].tsx
+++ pages/stocktake/template/[id].tsx
@@ -1,12 +1,13 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import Layout from 'components/layout'
-import StocktakeTemplateScreen from 'features/stock/stocktake/stocktake-template-screen'
+// import StocktakeTemplateScreen from 'features/stock/stocktake/stocktake-template-screen'
 // import { useRouter } from 'next/router'
 
 export default function StocktakePage() {
   // const router = useRouter()
   // const id = router.query.id
-  return <StocktakeTemplateScreen />
+  // return <StocktakeTemplateScreen />
+  return <div />
 }
 
 StocktakePage.getLayout = (page) => <Layout>{page}</Layout>
