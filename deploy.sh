646239a54577109b1ae66ac55ff9345f3ab9cb14
Ben <8870073+ben-dodd@users.noreply.github.com>
2023-11-14 23:44:02 +1300

    NVM loaded

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 cd /home/ubuntu/rideonsupersound
 git pull origin ross2.0
 npm install --legacy-peer-deps &&
