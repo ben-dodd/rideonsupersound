9832c7b85f572cf71e674610c92015ce62cac9cc
Ben <8870073+ben-dodd@users.noreply.github.com>
2024-03-01 23:38:09 +1300

    Removed stock list options

--- features/stock/stock-list/list.tsx
+++ features/stock/stock-list/list.tsx
@@ -1,24 +1,25 @@
-import { useAppStore } from 'lib/store'
-import { Pages } from 'lib/store/types'
-import StockListItem from './stock-list-item'
-import LoadMoreButton from 'components/button/load-more-button'
+// import { useAppStore } from 'lib/store'
+// import { Pages } from 'lib/store/types'
+// import StockListItem from './stock-list-item'
+// import LoadMoreButton from 'components/button/load-more-button'
 
-const StockListList = ({ stockItemList }) => {
-  const {
-    stockPage: { searchBar, limit },
-    setPage,
-  } = useAppStore()
+// const StockListList = ({ stockItemList }) => {
+//   const {
+//     stockPage: { searchBar, limit },
+//     setPage,
+//   } = useAppStore()
 
-  return (
-    <>
-      {stockItemList?.map((stockItem) => (
-        <StockListItem key={stockItem?.item?.id} stockListItem={stockItem} />
-      ))}
-      {limit < stockItemList?.length && (
-        <LoadMoreButton onClick={() => setPage(Pages.stockPage, { limit: (limit) => limit + 30 })} />
-      )}
-    </>
-  )
-}
+//   return (
+//     <>
+//       {stockItemList?.map((stockItem) => (
+//         <StockListItem key={stockItem?.item?.id} stockListItem={stockItem} />
+//       ))}
+//       {limit < stockItemList?.length && (
+//         <LoadMoreButton onClick={() => setPage(Pages.stockPage, { limit: (limit) => limit + 30 })} />
+//       )}
+//     </>
+//   )
+// }
 
-export default StockListList
+// export default StockListList
+export {}
--- features/stock/stock-list/sheet.tsx
+++ features/stock/stock-list/sheet.tsx
@@ -1,30 +1,32 @@
-import DataTable from 'components/data/data-table'
-import { getItemSku } from 'lib/functions/displayInventory'
+// import DataTable from 'components/data/data-table'
+// import { getItemSku } from 'lib/functions/displayInventory'
 
-const StockListSheet = ({ stockItemList, isLoading }) => {
-  const stockSchema = [
-    {
-      id: 'id',
-      key: 'id',
-      header: 'Stock ID',
-      getValue: (row) => row?.item?.id,
-    },
-    { id: 'vendorId', key: 'vendorId', header: 'Vendor ID', getValue: (row) => row?.item?.vendorId },
-    { id: 'sku', key: 'sku', header: 'SKU', getValue: (row) => getItemSku(row?.item), isLocked: true },
-    {
-      id: 'artist',
-      key: 'artist',
-      header: 'Artist',
-      getValue: (row) => row?.item?.artist,
-    },
-    {
-      id: 'title',
-      key: 'title',
-      header: 'Title',
-      getValue: (row) => row?.item?.title,
-    },
-  ]
-  return <DataTable initData={stockItemList} initSchema={stockSchema} isLoading={isLoading} />
-}
+// const StockListSheet = ({ stockItemList, isLoading }) => {
+//   const stockSchema = [
+//     {
+//       id: 'id',
+//       key: 'id',
+//       header: 'Stock ID',
+//       getValue: (row) => row?.item?.id,
+//     },
+//     { id: 'vendorId', key: 'vendorId', header: 'Vendor ID', getValue: (row) => row?.item?.vendorId },
+//     { id: 'sku', key: 'sku', header: 'SKU', getValue: (row) => getItemSku(row?.item), isLocked: true },
+//     {
+//       id: 'artist',
+//       key: 'artist',
+//       header: 'Artist',
+//       getValue: (row) => row?.item?.artist,
+//     },
+//     {
+//       id: 'title',
+//       key: 'title',
+//       header: 'Title',
+//       getValue: (row) => row?.item?.title,
+//     },
+//   ]
+//   return <DataTable initData={stockItemList} initSchema={stockSchema} isLoading={isLoading} />
+// }
 
-export default StockListSheet
+// export default StockListSheet
+
+export {}
