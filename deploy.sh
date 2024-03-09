b13930be65e53e5cae60d333a7e14755ba43bdc8
Ben <8870073+ben-dodd@users.noreply.github.com>
2024-03-09 13:37:39 +1300

    Pipeline works

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-cd /home/ubuntu/rideonsupersound
-git pull origin ross2.0
-npm install --legacy-peer-deps &&
-npm run build &&
-pm2 restart rideonsupersound
