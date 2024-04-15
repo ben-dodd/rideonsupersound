f109c8a4c35873473c8ad8263e9b18fad924bda7
Ben <8870073+ben-dodd@users.noreply.github.com>
2024-04-15 22:43:23 +1200

    Redirects only

--- components/_components/container/table.tsx
+++
@@ -1,9 +0,0 @@
-export default function TableContainer({ loading, children }) {
-  return loading ? (
-    <div className="loading-screen">
-      <div className="loading-icon" />
-    </div>
-  ) : (
-    <div className="h-menu w-board overflow-x-hidden bg-white">{children}</div>
-  );
-}
