69597330170fe5bde8a2cb5a1b3f81df883b0e59
Ben <8870073+ben-dodd@users.noreply.github.com>
2024-04-15 23:17:43 +1200

    ID debugs

--- components/vendor.tsx
+++ components/vendor.tsx
@@ -3,20 +3,19 @@ import { useEffect } from 'react'
 
 const Vendor = ({ id }) => {
   const router = useRouter()
+  console.log(id)
 
   useEffect(() => {
+    console.log(id)
     if (id) {
-      router.push(`https://vendor.rideonsupersound.co.nz/${id}`)
+      console.log('correct')
+      // router.push(`https://vendor.rideonsupersound.co.nz/${id}`)
     } else {
       console.error('ID is undefined or null')
     }
   }, [id, router])
 
   return <div></div>
-}
-
-Vendor.getInitialProps = ({ query }) => {
-  return { id: query.id }
 }
 
 export default Vendor
--- pages/vendor/[id].tsx
+++ pages/vendor/[id].tsx
@@ -4,5 +4,6 @@ import { useRouter } from 'next/router'
 export default function VendorPage() {
   const router = useRouter()
   const { id } = router.query
+  console.log(id)
   return <Vendor id={id} />
 }
