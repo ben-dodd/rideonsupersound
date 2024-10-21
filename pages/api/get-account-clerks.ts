457f313b0bef2edfa32eba0755e12d3bcb85c089
Ben <8870073+ben-dodd@users.noreply.github.com>
2024-10-22 10:54:43 +1300

    Remove not current clerks

--- pages/api/get-account-clerks.ts
+++ pages/api/get-account-clerks.ts
@@ -1,11 +1,11 @@
-import { NextApiHandler } from "next";
-import { query } from "../../lib/db";
+import { NextApiHandler } from 'next'
+import { query } from '../../lib/db'
 
 const handler: NextApiHandler = async (req, res) => {
-  const { account_id, k } = req.query;
+  const { account_id, k } = req.query
   try {
     if (k !== process.env.NEXT_PUBLIC_SWR_API_KEY)
-      return res.status(401).json({ message: "Resource Denied." });
+      return res.status(401).json({ message: 'Resource Denied.' })
     const results = await query(
       `
       SELECT *
@@ -15,15 +15,16 @@ const handler: NextApiHandler = async (req, res) => {
         FROM account_clerk
         WHERE account_id = ?
       )
+      AND is_current = 1
       ORDER BY colour
       `,
       account_id
-    );
+    )
 
-    return res.json(results);
+    return res.json(results)
   } catch (e) {
-    res.status(500).json({ message: e.message });
+    res.status(500).json({ message: e.message })
   }
-};
+}
 
-export default handler;
+export default handler
